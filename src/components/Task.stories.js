75769eeef04ddf8a4749d257d1a5548940386fb6
Nicholas Bester <nick.bester@hellocomputer.com>
2020-01-24 13:10:37 +0200

    Fixed Task story

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -2,6 +2,16 @@ import { action } from '@storybook/addon-actions';
 import { withKnobs, object } from '@storybook/addon-knobs';
 import Task from './Task';
 
+export const taskData = {
+	id: '1',
+	title: 'Test Task',
+	state: 'Task_INBOX',
+	updated_at: new Date(2019, 0, 1, 9, 0)
+};
+const longTitle = "This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!";
+
+const taskTemplate = `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`;
+
 export default {
 	title: 'Task',
 	decorators: [withKnobs],
@@ -12,8 +22,6 @@ export const actionsData = {
 	onPinTask: action('onPinTask'),
 	onArchiveTask: action('onArchiveTask')
 };
-
-const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 
 // default task state
 export const Default = () => ({
@@ -67,65 +75,3 @@ export const LongTitle = () => ({
   },
   methods: actionsData,
 });
-
-export const taskData = {
-	id: '1',
-	title: 'Test Task',
-	state: 'Task_INBOX',
-	updated_at: new Date(2019, 0, 1, 9, 0)
-};
-
-const taskTemplate = `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`;
-
-// default task state
-export const Default = () => ({
-	components: { Task },
-	template: taskTemplate,
-	props: {
-		task: {
-			default: object('task', { ...taskData })
-		}
-	},
-	methods: actionsData
-});
-// pinned task state
-export const Pinned = () => ({
-	components: { Task },
-	template: taskTemplate,
-	props: {
-		task: {
-			default: {
-				...taskData,
-				state: 'TASK_PINNED'
-			}
-		}
-	},
-	methods: actionsData
-});
-// archived task state
-export const Archived = () => ({
-	components: { Task },
-	template: taskTemplate,
-	props: {
-		task: {
-			default: {
-				...taskData,
-				state: 'TASK_ARCHIVED'
-			}
-		}
-	},
-	methods: actionsData
-});
-export const LongTitle = () => ({
-	components: { Task },
-	template: taskTemplate,
-	props: {
-	  task: {
-		default: {
-		  ...taskData,
-		  title: longTitle,
-		},
-	  },
-	},
-	methods: actionsData,
-  });
